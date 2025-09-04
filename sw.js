127287cf5fd7483fe85072ec6b93511bf9c4adc2
Andrew Nixdorf <nixdorfa@gmail.com>
2025-09-04 01:07:13 -0700

    Fix path in js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
 const CACHE = "andrewnixdorf-portfolio-v1";
-const ASSETS = ["/","/index.html","/css/style.css","/js/app.js","/data/site.config.json","/assets/logo.svg"];
+const ASSETS = ["/","./index.html","./css/style.css","./js/app.js","./data/site.config.json","./assets/logo.svg"];
 self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
 self.addEventListener("fetch",(e)=>{e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));});
